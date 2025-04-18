0ebc4cb3b5586490fb744d2a2ec258830300e274
Giovanni-Paone <paonegiovanni04@gmail.com>
2025-04-18 15:56:07 +0200

    Inizio progetto

---
+++ src/main/java/model/Hackathon.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+//.
+public class Hackathon {
+    public final String TITOLO;
+    public final String SEDE;
+    public final Date INIZIO;
+    public final Date FINE;
+    public int iscritti;
+    public final int MAX_ISCRITTI;
+    public final int MAX_TEAM_SIZE;
+    public ArrayList<Team> classificati = new ArrayList<Team>();
+    public short[] votiClassificati;
+
+    public Hackathon(String TITOLO, String SEDE, Date INIZIO, Date FINE, int MAX_ISCRITTI, int MAX_TEAM_SIZE) {
+        this.TITOLO = TITOLO;
+        this.SEDE = SEDE;
+        this.INIZIO = INIZIO;
+        this.FINE = FINE;
+        this.iscritti = 0;
+        this.MAX_ISCRITTI = MAX_ISCRITTI;
+        this.MAX_TEAM_SIZE = MAX_TEAM_SIZE;
+    }
+}
